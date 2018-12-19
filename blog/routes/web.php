68f1ff72ee44383b188db4109aae6988e6928db6
mirindaunicorn <panic.ability@gmail.com>
2018-12-19 23:16:07 +0200

    add admin-panel

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function (){
+    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
